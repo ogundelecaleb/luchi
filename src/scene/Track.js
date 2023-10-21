6e2610045d41d3e704ed97170322b9237ad18cee
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-10-21 23:10:25 +0100

    update

--- src/scene/Track.js
+++ src/scene/Track.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Track = () => {
   return (
-    <div>Charges</div>
-  )
-}
+    <div>
+      {" "}
+      <div className="px-[16px] md:px-[24px] lg:px-[32px] py-[32px]">
+      <h3 className="flex items-center text-[18px] md:text-[24px] lg:text-[28px] font-bold text-[#29361C]">
+      Track your Food Order <img src="/hotfood.png" alt="hot food" className="h-[28px] " />
+          </h3>
+      </div>
+    </div>
+  );
+};
 
-export default Track
+export default Track;
